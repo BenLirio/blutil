5d2d6ed568daa399d490008d5fd2260315b5dacf
Ben Lirio <benjlirio@gmail.com>
2020-10-18 18:22:54 -0400

    Automated builds work for now

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,3 @@ cd ./cli-gen
 go build
 ./cli-gen
 cd ..
-# Manually build utils
-go build -v ./cmd/util
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,5 @@
+# Manually build utils
+go build -v ./cmd/util
+gsutil rm gs://lirio-tools-release/util
 gsutil cp ./util gs://lirio-tools-release/util
+rm ./util
